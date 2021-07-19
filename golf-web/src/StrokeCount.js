d655c233f9c0e9816de6461cd54b7a0d08c39098
Marcelo-Zometa <marzometa21@gmail.com>
2021-07-19 16:17:55 -0600

    Added working submit button

--- golf-web/src/StrokeCount.js
+++ golf-web/src/StrokeCount.js
@@ -9,12 +9,19 @@ function StrokeCount() {
         console.info("Number of holes changed to " + numberOfHoles);
     }
 
+    const HandleSubmit = () => {
+        console.info("Number passed is " + numberOfHoles);
+    }
+
     return (
         <div className="App">
         <header className="App-header">
-            <h2>How many holes are you playing?</h2>     
-            <h4>{numberOfHoles}</h4>
-            <InputNumber setFunction={SetNumberOfHoles}/>
+            
+        <h2>How many holes are you playing?</h2>     
+        <h4>{numberOfHoles}</h4>
+        <InputNumber setFunction={SetNumberOfHoles}/>
+        <button type='submit' onClick={HandleSubmit}>Submit</button>
+       
         </header>
         </div>
   );
